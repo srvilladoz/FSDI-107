7e42c03cbcd6d1fadf6116161db580a1eedab1fe
srvilladoz <85268801+srvilladoz@users.noreply.github.com>
2021-08-06 20:39:55 -0700

    Add files via upload

---
+++ catalog.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Item from "./item";
+import ItemService from "../services/itemService";
+
+
+import "./catalog.css";
+
+class Catalog extends Component {
+    state = {
+        catalog: []
+    };
+
+    render() {
+        return (
+            <div className="catalog">
+                <h3>Our Amazing Catalog</h3>
+                <h5>We have { this.state.catalog.length } products for you</h5>
+
+                { this.state.catalog.map( obj => <Item key={obj._id} data={obj}></Item>)}
+            </div>
+        );
+    }
+
+    //best place to load data from server
+    //executed after the initial render
+
+    componentDidMount () {
+
+        let service = new ItemService();
+        let data = service.getCatalog();
+        this.setState({ catalog: data });
+    }
+}
+
+export default Catalog;
